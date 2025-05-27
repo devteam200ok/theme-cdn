13172a64f95c31f1d15bed314556fccb56cb0436
haerae <114662990+devteam200ok@users.noreply.github.com>
2025-05-27 16:23:39 +0700

    update

---
+++ phoenix/assets/js/Perfection_is_in_the_details.js
@@ -0,0 +1,14 @@
+console.log(
+    "%c     __         __               \n" +
+    "%c ___/ /__ _  __/ /____ ___ ___ _ \n" +
+    "%c/ _  / -_) |/ / __/ -_) _ `/  ' \\ \n" +
+    "%c\\_,_/\\__/|___/\\__/\\__/\\_,_/_/_/_/ \n" +
+    "%c                                  \n" +
+    "%c★ https://devteam-works.com/career ★\n",
+    "color: #46ba81; font-size: 11px;", // start of gradient (green)
+    "color: #379672; font-size: 11px;", // intermediate color
+    "color: #2a725a; font-size: 11px;", // intermediate color
+    "color: #1d4d43; font-size: 11px;", // intermediate color
+    "color: #122438; font-size: 11px;",  // end of gradient (navy)
+    "color: #000000; font-size: 11px;"  // black text
+);
